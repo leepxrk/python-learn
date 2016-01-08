cbb8f1025cce6eb1c493df0b0a013f9051b200df
leepxrk <leepx01rk@hotmail>
2016-01-08 23:34:19 +0800

    january_9_2016

--- learn_str/learn_str_function.py
+++ learn_str/learn_str_function.py
@@ -3,7 +3,30 @@
 
 import string
 
-title = 'whit a moo-moo here here. and a moo-moo there'
+title = 'WHIT A MOO-MOO here here. and a moo-moo there'
+seq = [1,2,3,4,5]
+sep = '+'
 
 #find 方法在一个较长的字符串中查找子字符串，返回子字符串最左侧的索引，无则返回-1
 print title.find(here)
+
+#join 字符串中非常重要的方法，是split方法的逆方法，用于连接序列中的元素
+print seq.join(sep)
+
+#lower  返回字符串的小写字母版
+print title.lower()
+
+#replace  返回某字符串的所有匹配项军备替换之后得到的字符串
+print title.replace('moo','mee')
+
+#split
+
+
+#strip
+
+
+#translate
+
+
+
+#
