51e226e6847c304722d1fe07021c619da0b727c3
leepxrk <leepx01rk@hotmail>
2016-01-09 23:45:09 +0800

    更新部分字典信息

---
+++ learn_zd/learn_zd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#字典中，前面的字符串称为键，后面的字符串称为值。
+#键是唯一的，值可以不唯一
+
+#dict 函数  建立字典
+itemt = [('name','gumby'),('age','42')]
+d = dict(itemt)
+print d
+
+{'age':42,'name':'gumby'}
+print d['name']
