ca57e1dd31f4cf4c15c74ce1f7ed2d7930f1fb3d
leepxrk <leepx01rk@hotmail>
2016-01-19 08:37:45 +0800

    19-1-2016

---
+++ learn_xh/learn_xh_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#使用print 打印多个表达式，需要讲不同表达式之间使用逗号进行分隔
+#在结果中输入逗号，可以采用以下方式
+import sys,somemodule
+a = 'hello'
+b = 'world'
+
+print a + ',' + b
+
+#将两句语句在同一行进行打印，需要将一个函数结尾出加上逗号
+print a,
+print b
+
+
+#把某件事作为另一件事导入
