5a5d0ef684e0af5721d7ecad119c1ba0e45427ae
leepxrk <leepx01rk@hotmail.com>
2018-04-21 13:44:18 +0800

    111

--- learn_algorithm/fbnqsl.py
+++ learn_algorithm/fbnqsl.py
@@ -4,25 +4,37 @@
 # 记录400万以内的最大斐波那契数，同时求出这个数是第几个斐波那契数
 
 #定义变量
-import random,MySQLdb,sys
+import random,sys
+
 
 n = int(input("数值A:"))
 a = 0
 b = 1
 c = 1
 
+q = random.randint(1,5)
+
+Fbnqsl=[]
+
 while c < n:
-        a = b + c
-        if a < n:
-            b = a + c
-        else:
-            print c
-            break
-        if b < n:
-            c = a + b
-        else:
-            print a
-            break
-        if c > n:
-            print b
-            break
+    a = b + c
+    if a < n:
+        b = a + c
+        Fbnqsl.append(c)
+        Fbnqsl.insert(q,a)
+    else:
+        print(c)
+        break
+    if b < n:
+        c = a + b
+        Fbnqsl.append(b) 
+    else:
+        print(a)
+        break
+    if c > n:
+        print(b)
+        break
+
+print(Fbnqsl)
+
+#print (random.random())
