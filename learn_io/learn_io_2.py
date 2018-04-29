d161f7cd965bbbf0a61057ae8295ed6fad7087b9
leepxrk <leepx01rk@hotmail.com>
2018-04-29 23:43:32 +0800

    1

--- learn_io/learn_io_2.py
+++ learn_io/learn_io_2.py
@@ -2,11 +2,14 @@
 # -*- coding: UTF-8 -*-
 
 # 导入 Phone 包
-import Phone
- 
-Phone.Pots()
-Phone.Isdn()
-Phone.G3()
+#import Phone
+import pymysql
+import pymysql.cursors
+
+
+#Phone.Pots()
+#Phone.Isdn()
+#Phone.G3()
 
 
 
@@ -17,5 +20,17 @@ Phone.G3()
 #    print 'hello:',par
 #    return
 
+# lianjie shujuku
+conn = pymysql.connect(host = 'localhost',port = '3306',user = 'root',password = 'qq19900807',db = 'mysql',charset = 'utf-8')
 
+cursor = db.cursor()
+
+# 运行查询:
+cursor = conn.cursor()
+cursor.execute('select * from user where host = localhost & user = root limit 2', ('1',))
+values = cursor.fetchall()
+values
+[('1', 'Michael')]
+# 关闭Cursor和Connection:
+cursor.close()
 
