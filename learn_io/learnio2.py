86f135d58e89aeb1902e7d83665e3449a8579ed5
leepxrk <leepx01rk@hotmail.com>
2018-06-11 22:33:29 +0800

    ddd

--- learn_io/learnio2.py
+++ learn_io/learnio2.py
@@ -20,7 +20,7 @@ import csv
 
 
 # 读取整个csv文件
-with open('shanghai/600000.csv','r') as csv_file:
+with open('shanghai/600000.csv','rb') as csv_file:
     csv_reader = csv.reader(csv_file,1,None)
     for row in csv_reader:
         print(row)
@@ -62,3 +62,54 @@ print(b)
 #    print("nickName:%s\sex:%.2f" % row)
 
 #print(cursor.execute(QueryData % condition))
+
+
+
+
+
+
+ def insert_inspection_list(self,table_name):
+        for i in range(1,100):
+
+            id = str(i)
+            inspection_num = 'NJ'+ str(100000+i)
+            car_id =  i+1
+            
+            create_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+            update_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+
+            #@@@@@字符串用双引号加单引号"''",三种sql语句
+           sql1 = 'insert into car (id,inspection_num,car_id, create_uid) values 
+('+id+',"' + inspection_num + '",' + car_id + ',238);           
+           
+           sql2 = 'insert into car (id,inspection_num,car_id,create_uid，create_time,
+update_time)values({},{},{},{},{},{})'.format(id,inspection_num,car_id,238,create_time,
+update_time)
+
+           sql3 = 'insert into car (id,inspection_num,car_id,create_uid，create_time,
+update_time)values (%s,%s,%d,%s,%s,%s)' % (id, inspection_num, car_id, 238, create_time,
+ update_time)
+            print sql
+            cursor = self.conn.cursor()
+
+            cursor.execute(sql)
+            self.conn.commit()
+
+
+    # 关闭数据库
+    def close(self):
+        self.conn.close()
+
+
+if  __name__ == '__main__':
+    tb = DataBase('manager')
+    tb.insert_inspection_list('inspection_list')
+
+
+
+
+
+
+
+
+
--- learn_io/learnio_csv_to_db.py
+++ learn_io/learnio_csv_to_db.py
@@ -1,35 +1,77 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import pymysql
+import pymysql,csv
 import pymysql.cursors
 
 
-# lianjie shujuku
-linkDB = pymysql.connect(host='127.0.0.1',port =3306,user='root',password='qq19900807',database='DBLearnT',charset='utf8')
+# 连接数据库
+linkDB = pymysql.connect(host='127.0.0.1',port =3306,user='root',password='19900807',database='learn_data',charset='utf8')
+
+# 使用cursor方法创建游标
+cur = linkDB.cursor()
 
 
-# chuangjianyoubiao
-cursor = linkDB.cursor()
+#创建空列表
+A_share_date = []
+
+# 打开csv文件
+csv_date = open('shanghai/600000.csv','rb')
+f = csv_date.readline().splitlines() #仅读取一行数据
+
+print(f)
+
+A_share_date.append(f)
+
+print(A_share_date)
+
+# 写入数据
+update_db = "INSERT INTO A_share_shanghai_bourse VALUES()"
 
 
-#charushuju
-QueryData = "select nickName,sex from tme_musician where id > '%s' limit 10"
-condition = ('10000',)
-cursor.execute(QueryData % condition)
-
-for row in cursor.fetchall():
-    print("nickName:%s\sex:%.2f" % row)
-
-print(cursor.execute(QueryData % condition))
-
-
-# 运行查询:
-#sql = "select * from tme_musician"
-#a = cursor.execute(sql)
-#print(a)
+# A_share_date.append(f)
 
 
 
-cursor.close()
+
+# 将一行数据写入列表
+
+
+
+
+# 查询数据行数
+QueryData = "select id,start_time from A_share_shanghai_bourse where stock_code = 'f' limit 10"
+cur.execute(QueryData)
+date = cur.fetchall()
+for id,start_time in date:
+    print(id,start_time)
+
+
+
+
+# 查询
+#QueryData = "select id,start_time from A_share_shanghai_bourse where stock_code = '600006' limit 10"
+#cur.execute(QueryData)
+#date = cur.fetchall()
+#for id,start_time in date:
+#    print(id,start_time)
+
+
+#关闭数据库连接
+
 linkDB.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
