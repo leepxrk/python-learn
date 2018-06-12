c758e19db83b6d9d0ce7eb4977a917a13cd8653d
leepxrk <leepx01rk@hotmail.com>
2018-06-12 22:08:52 +0800

    121221

--- learn_io/learnio_csv_to_db.py
+++ learn_io/learnio_csv_to_db.py
@@ -19,14 +19,31 @@ A_share_date = []
 csv_date = open('shanghai/600000.csv','rb')
 f = csv_date.readline().splitlines() #仅读取一行数据
 
+print(f)
+
+stock_code = f[0]
+start_time = f[1]
+stop_time = f[2]
+stock_turnover = f[3]
+stock_trading_volume = f[4]
+opening_price = f[5]
+low = f[6]
+high = f[7]
+closing_price = f[8]
+opening_front_subscription_price = f[9]
+low_front_subscription_price = f[10]
+high_front_subscription_price = f[11]
+closing_front_subscription_price = f[12]
+opening_back_Subscription_price = f[13]
+low_back_Subscription_price = f[14]
+high_back_Subscription_price = f[15]
+closing_back_Subscription_price = f[16]
+
+
+
 
 # 写入数据
-update_db = "INSERT INTO A_share_shanghai_bourse VALUES()"
-
-
-# A_share_date.append(f)
-
-
+update_db = """INSERT INTO A_share_shanghai_bourse (stock_code,start_time,stop_time,stock_turnover,stock_trading_volume,opening_price,low,high,closing_price,opening_front_subscription_price,low_front_subscription_price,high_front_subscription_price,closing_front_subscription_price,opening_back_Subscription_price,low_back_Subscription_price,high_back_Subscription_price,closing_back_Subscription_price)VALUES(%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d)",(stock_code,start_time,stop_time,stock_turnover,stock_trading_volume,opening_price,low,high,closing_price,opening_front_subscription_price,low_front_subscription_price,high_front_subscription_price,closing_front_subscription_price,opening_back_Subscription_price,low_back_Subscription_price,high_back_Subscription_price,closing_back_Subscription_price)"""
 
 
 # 将一行数据写入列表
@@ -35,7 +52,7 @@ update_db = "INSERT INTO A_share_shanghai_bourse VALUES()"
 
 
 # 查询数据行数
-QueryData = "select id,start_time from A_share_shanghai_bourse where stock_code = 'f' limit 10"
+QueryData = "select id,start_time from A_share_shanghai_bourse where stock_code = '600000' limit 10"
 cur.execute(QueryData)
 date = cur.fetchall()
 for id,start_time in date:
