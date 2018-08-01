19f0cc74b16a90f69f78a6a99c9a4ef0025416ac
leepxrk <leepx01rk@hotmail.com>
2018-08-01 22:01:17 +0800

    ddadadada

--- geek_time/file_op.py
+++ geek_time/file_op.py
@@ -2,15 +2,6 @@
 
 # 文件的基本操作
 
-import csv,os
-
-#file1 = open('600000.csv')
-
-#with open(r'C:\Users\leepx\OneDrive\Myself\GitHub\python_learn\geek_time\600000.csv') as file2:
-#    file_read = file2.read()
-#    print (file_read)
-
-
 # 将名称“刘备”写入文件
 # 写入的操作标记w，在文本末尾的操作标记a
 
