999484702f6ca3152c702ef280380ae2b416f051
rongke <rongke@imagedt.com>
2019-06-05 12:57:23 +0800

    更新数据库连接地址

--- crawler_learn/xxpythondzqzz/linkMysqlDB.py
+++ crawler_learn/xxpythondzqzz/linkMysqlDB.py
@@ -2,7 +2,7 @@
 import pymysql
 
 # 使用 connect 方法，传入数据库地址，账号密码，数据库名就可以得到你的数据库对象
-db = pymysql.connect("location", "root", 
+db = pymysql.connect("172.30.0.12", "root", 
 "19900807", "avIdol")
 
 # 接着我们获取 cursor 来操作我们的 avIdol 这个数据库
