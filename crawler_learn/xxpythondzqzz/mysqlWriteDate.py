664e5952a0e6f72d203b66a4e83dd9931f0ea29a
rongke <rongke@imagedt.com>
2019-06-05 13:05:03 +0800

    写入数据

---
+++ crawler_learn/xxpythondzqzz/mysqlWriteDate.py
@@ -0,0 +1,28 @@
+
+import pymysql
+
+# 使用 connect 方法，传入数据库地址，账号密码，数据库名就可以得到你的数据库对象
+db = pymysql.connect("127.0.0.1", "root", 
+"19900807", "avIdol")
+
+
+import pymysql
+
+# 使用 connect 方法，传入数据库地址，账号密码，数据库名就可以得到你的数据库对象
+db = pymysql.connect("localhost", "root", "root", "avIdol")
+
+# 接着我们获取 cursor 来操作我们的 avIdol 这个数据库
+cursor = db.cursor()
+
+# 插入一条记录
+sql = "insert into beautyGirls(name, age) values ('Mrs.cang', 18)"
+
+try:
+    cursor.execute(sql)
+    db.commit()
+except:
+    # 回滚
+    db.rollback()
+
+# 最后我们关闭这个数据库的连接
+db.close()
