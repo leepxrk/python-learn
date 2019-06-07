c890eb438a2972469cae91727ef6afc4316475e2
rongke <rongke@imagedt.com>
2019-06-07 14:47:02 +0800

    5463hdsvgsefg

--- learn/learn_list/paixu_learn.py
+++ learn/learn_list/paixu_learn.py
@@ -10,11 +10,11 @@ paixu = []
 # paixu.insert(0,68)   #insert方法有两个参数，第一个参数是代表函数插入的索引位，第二个参数代表插入的元素
 
 
-x = 1
-while x <= 5:
-    paixu.append(x)
-    print(x)
-    x += 1
+#x = 1
+#while x <= 5:
+#    paixu.append(x)
+#    print(x)
+#    x += 1
 
 
 
@@ -23,6 +23,7 @@ y = len(paixu)
 
 while y < 7:
     paixu.append(input("输入数值："))
+    print(y)
     y = len(paixu)
 
 paixu.sort()
