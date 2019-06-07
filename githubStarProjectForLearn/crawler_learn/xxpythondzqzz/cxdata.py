21814766ea5f9275c496b31551b4820d7ee3ae4b
leepxrk <leepx01rk@hotmail.com>
2019-06-07 20:45:00 +0800

    更新查询条件

--- githubStarProjectForLearn/crawler_learn/xxpythondzqzz/cxdata.py
+++ githubStarProjectForLearn/crawler_learn/xxpythondzqzz/cxdata.py
@@ -1,22 +1,25 @@
 
-import pymysql
+import pymysql 
 
 
 # 创建空列表
 datalist = []
 
 # 使用 connect 方法，传入数据库地址，账号密码，数据库名就可以得到你的数据库对象
-db = pymysql.connect("127.0.0.1", "root", 
+db = pymysql.connect("94.191.94.133", "root", 
 "19900807", "statisticalLearning")
 
 # 接着我们获取 cursor 来操作我们的 statisticalLearning 这个数据库
 cursor = db.cursor()
 
+queryStockCode = input("请输入需要查询的股票代码：")
+limitCode = input("请输入需要查询的数量：")
+
 # 插入一条记录
 # sql = "insert into beautyGirls(name, age) values ('Mrs.cang', 18)"
 # sql = "select low,high from stockData where stockCode = '600000'"
 
-cursor.execute("select high from stockData where stockCode = '600000' limit 3")
+cursor.execute('select high from stockData where stockCode = %s limit %s',(queryStockCode,limitCode))
 alldata = cursor.fetchall()
 
 
