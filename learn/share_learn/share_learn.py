6d595619cf024e86a9ff328a29d58fd7ac118442
leepxrk <leepx01rk@hotmail.com>
2019-06-22 23:32:58 +0800

    更新整个内容

--- learn/share_learn/share_learn.py
+++ learn/share_learn/share_learn.py
@@ -1,46 +1,39 @@
+# 引入sys&io&importlit,并确认文件的编码格式
+import io
+import sys
+import importlib
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+importlib.reload(sys)
+
+
+# 引入pandas&sqlalchemy&pymysql用于操作数据和写入数据库
 import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
 
-import io
-import sys
-
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
-
-
-import sys
-import importlib
-importlib.reload(sys)
- 
-
-
-
-
-#   引入 tushare
-import tushare as ts
-
-# 引入pymysql
-import pymysql
 
 # 使用 connect 方法，传入数据库地址，账号密码，数据库名就可以得到你的数据库对象
-db = pymysql.connect("127.0.0.1", "root", 
+con = pymysql.connect("127.0.0.1", "root", 
 "19900807", "statisticalLearning")
 
 
-# 设置token
+# 引入 tushare 用于获取股市交易资料
+import tushare as ts
+
+
+# 设置token 用于通过tushare提供的API获取股市交易资料
 ts.set_token('41f23d83224913cd79af2c386919c0b84c23d46d6a798890eecc53ee')
 
-# 初始化api
+# 初始化api（tushare提供的初始化API的方案）
 # pro = ts.pro_api()
 
 
 # 获取股票列表数据
 stock = ts.get_stock_basics()
-
 # print (stock)
 
-
-stock.to_sql(name='stock_basic',con = con,if_exists='append', index=False)
+# 将获取到的数据写入数据库
+stock.to_sql(name='stock_basic',con = connect,if_exists='append',index=False)
 
 
 
