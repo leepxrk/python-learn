e2db9bfcf324f36bfad60cb2f076f32e77855de5
root <root@localhost.localdomain>
2019-06-22 20:52:08 +0800

    add new files

--- learn/learn_algorithm/tushare_test.py
+++ learn/learn_algorithm/tushare_test.py
@@ -1,7 +1,22 @@
-import tushare
+import tushare as ts
 
 # 查询当前所有正常上市交易的股票列表
-pro = ts.pro_api()
+# pro = ts.pro_api()
 
 
-data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+# data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+
+ts.get_hist_data('600848',start='2015-01-05',end='2015-01-09')
+
+# ts.get_hist_data('600848') #一次性获取全部日k线数�
+
+
+
+
+
+ts.new_stocks()
+
+
+
+
+
